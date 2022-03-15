87b369b9fe06e1ae6243b034fdd09da29e3f2e37
RavelloH <68409330+RavelloH@users.noreply.github.com>
2022-03-15 21:25:00 +0800

    Create USBhelper.bat

---
+++ USBhelper.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Running...
+dir ./ /s > ./dir.txt
+echo Porgross:1/2
+tree ./ /s > ./menu.txt 
+echo Progross:2/2
+echo Already finish all job.
+echo Press any button to exit.
+pause
